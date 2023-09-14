f028b9b412e6d565048ee533af178b60ff95c28e
jesseagleboy <jslomowitz@gmail.com>
2023-09-14 13:30:59 -0400

    try new controllers

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
         respond_to do |format|
             format.html {
                 if user_signed_in?
-                    redirect_to 'http://localhost:4321/', allow_other_host: true
+                    redirect_to 'https://rails-to-break.vercel.app/', allow_other_host: true
                 else
                     redirect_to new_user_session_path
                 end
@@ -67,7 +67,7 @@ class PostsController < ApplicationController
             respond_to do |format|
                 format.html {
                     flash[:notice] = "Post was successfully created."
-            redirect_to 'http://localhost:4321/', allow_other_host: true
+            redirect_to 'https://rails-to-break.vercel.app/', allow_other_host: true
                 }
                 format.json { render json: {post: @post, current_user: current_user, user_signed_in: user_signed_in?} }
             end
@@ -78,7 +78,7 @@ class PostsController < ApplicationController
 
     def manual_logout
         sign_out current_user
-        redirect_to 'http://localhost:4321'
+        redirect_to 'https://rails-to-break.vercel.app/'
     end
     def manual_sign_in
         redirect_to new_user_session_path
